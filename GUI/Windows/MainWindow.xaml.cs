fc3005898a18582258cc15bcef5a82d6a8a48f46
C-Fergus <59059263+C-Fergus@users.noreply.github.com>
2021-01-24 17:01:29 +0000

    move styling to resources and windows to subfolders
    add click action to all buttons
    add checked action to checkboxes
    rename project to "WindowsShiteSieve"

---
+++ GUI/Windows/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WindowsShiteSieveGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadConfig_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SaveConfig_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void SaveRun_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UWPApps_Click(object sender, RoutedEventArgs e)
+        {
+            new UWPApps().Show();
+        }
+
+        private void OtherApps_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void AllUsers_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AllUsers_Unchecked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GeneralSettings_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RegistryEdits_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void StartMenu_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Tasks_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void WindowsExplorer_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RevertAllChanges_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RevertRegistryEdits_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RevertSpecificChange_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
--- GUI/Windows/UWPApps.xaml.cs
+++ GUI/Windows/UWPApps.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WindowsShiteSieveGUI
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for UWPApps.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class UWPApps : Window
     {
-        public MainWindow()
+        public UWPApps()
         {
             InitializeComponent();
         }
